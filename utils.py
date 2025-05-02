8e8778ce2f5933d6ce03609bbe177a480c469009
zouge666 <1615379639@qq.com>
2025-05-02 22:11:16 +0200

    feat/function

--- utils.py
+++ utils.py
@@ -12,4 +12,7 @@ def get_vendor_name(vendor_id):
         cursor = conn.cursor()
         cursor.execute("SELECT name FROM Vendors WHERE vendor_id = ?", (vendor_id,))
         row = cursor.fetchone()
+        # Get the data on behalf of Vendor and Platform
+        # if_acts_for(get_vendor_name, {V,P})
+        # vendorName := declassify(vendorName, {V,P:{⊥}})
         return row[0] if row else None
